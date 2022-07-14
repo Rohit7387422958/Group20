20e12365cb3ae7706581f0bfacd20c201a39e5c2
Rohit7387422958 <chandguderohit25@gmail.com>
2022-07-14 16:21:15 +0530

    Hi good Morining

--- src/BasicJavaCode/MyClass.java
+++ src/BasicJavaCode/MyClass.java
@@ -5,6 +5,9 @@ public class MyClass {
 	public static void main(String[] args) 
 	{
 	System.out.println("hi good moring");
+    System.out.println("my first commit");
+	System.out.println("Hi good Morining");
+	
 	}
     
 
